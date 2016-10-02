cc01f460c784e27469c54644dd18eaa48d64746a
Joanne Lee <jol155@ucsd.edu>
2016-10-02 01:07:54 -0700

    Monitor controller to switch slides

---
+++ Assets/Scripts/UI/MonitorController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonitorController : MonoBehaviour {
+
+    private GameObject[] slides;
+    public int NUMSLIDES = 8;
+    public GameObject currentActiveSlide = null;
+
+	// Use this for initialization
+	void Start () {
+
+        slides = new GameObject[NUMSLIDES];
+
+        for (int i = 0; i <  NUMSLIDES; i++)
+        {
+            slides[i] = GameObject.Find("Canvas").transform.Find("Instruction Panel " + (i + 1)).gameObject;
+        }
+	}
+	
+    public void showSlide (int slideNum)
+    {
+        if ((slideNum >= NUMSLIDES) || (slideNum < 0))
+        {
+            Debug.Log("slideNum is OOB");
+            return;
+        }
+
+        if (currentActiveSlide != null)
+        {
+            currentActiveSlide.SetActive(false);
+        }
+
+        else
+        {
+            Debug.Log("no active slide");
+            return;
+        }
+
+        currentActiveSlide = slides[slideNum];
+        currentActiveSlide.SetActive(true);
+    }
+}
